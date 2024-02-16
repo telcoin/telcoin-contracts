01b92b8ad969af5bef19f4ace31f97853d05e157
Amir Shirif <31713018+amshirif@users.noreply.github.com>
2024-02-16 10:24:33 -0600

    Finish documentation

--- contracts/swap/interfaces/ISimplePlugin.sol
+++ contracts/swap/interfaces/ISimplePlugin.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 interface ISimplePlugin is IERC165 {
+    /**
+     * @notice provides rewards
+     * @param account the address to get the telcoin
+     * @param amount the telcoin value that is being added
+     */
     function increaseClaimableBy(
         address account,
         uint256 amount
